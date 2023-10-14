585eaa84246bb5655c123258d3915f1e6ae724a8
IzaanAnwar <mdizaan67@gmail.com>
2023-10-14 22:08:41 +0530

    side panel done

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -7,51 +7,51 @@ import PromoteIcon from "@mui/icons-material/SupervisedUserCircleOutlined";
 import HelpIcon from "@mui/icons-material/LiveHelpOutlined";
 const SidePanel = () => {
   return (
-    <div className="text-sm w-1/2 md:w-[24%] lg:w-[20%] bg-accent-primary min-h-screen text-left text-light-text-sc p-4 pt-8 md:pt-4 lg:pt-4 flex flex-col justify-between overflow-y-auto">
+    <div className="text-sm w-1/2 md:w-[24%] lg:w-[20%] bg-accent-primary min-h-screen text-left text-light-text-sc p-4 flex flex-col justify-between">
       <div>
         <div className="font-semibold py-2 px-4 text-light-text-pr flex justify-start items-center">
           <SettingsIcon />
           <h2 className="px-2 text-lg ">Dashboard</h2>
         </div>
-        <div className="flex justify-start items-center py-2 rounded-md px-4 hover:bg-accent-hover ">
+        <div className="flex justify-start items-center py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100 ">
           <DashboardIcon />
           <a href="#" className="px-2">
             Dashboard
           </a>
         </div>
 
-        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover">
+        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100">
           <ProductsIcon />
           <a href="#" className="px-2">
             Products
           </a>
         </div>
-        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover">
+        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100">
           <CustomersIcon />
           <a href="#" className="px-2">
             Customers
           </a>
         </div>
-        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover">
+        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100">
           <IncomeIcon />
           <a href="#" className="px-2">
             Income
           </a>
         </div>
-        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover">
+        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100">
           <PromoteIcon />
           <a href="#" className="px-2">
             Promote
           </a>
         </div>
-        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover">
+        <div className="flex justify-start py-2 rounded-md px-4 hover:bg-accent-hover hover:text-light-text-pr duration-100">
           <HelpIcon />
           <a href="#" className="px-2">
             Help
           </a>
         </div>
       </div>
-      <div className="rounded-md px-4 py-2 flex justify-start items-center hover:bg-accent-hover">
+      <div className="rounded-md px-4 py-2 flex justify-start items-center hover:bg-accent-hover hover:text-light-text-pr duration-100">
         <img
           src="/dummy-usr.png"
           alt="User"
